f7218a50c1b83ab3ac49208ac7dcef51bc36f585
augustin <grahaugustin@gmail.com>
2021-05-06 23:50:40 +0000

    Ajout d'ancètre et pere de manière statique

--- classObeject.rkt
+++ classObeject.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-;;;;;AUTHOR :AUGUSTIN GRAH
+;******************* :AUGUSTIN GRAH*********************
+;*******************git:augustCalibre*******************
 
 (define BC '(
              Homme(toto)
@@ -33,8 +34,8 @@
     
       ;(define tru #t)(define false #f)
     
-      (define/public (toto x)(eqv? x "?" )))
-   )
+      (define/public (toto x)(eqv? x "?" ))
+   ))
 
 
 (define Famille%
@@ -42,20 +43,100 @@
     (super-new)
   
     (define/public (parent x y)
-            (eqv? x y))))
+            (eqv? x y))
 
 
-  (define/public (parent x y)
-            (eqv? x y)
+  (define/public (enfants x y)
+            (eqv? x y))
+
+  (define/public (pere x y)
+            (eqv? x y))
+
+
+  (define/public (mere x y)
+            (eqv? x y))
+
+   (define/public (ancetre x y)
+            (eqv? x y))
+  
+   (define/public (grandParents x y)
+            (eqv? x y))
+
+   (define/public (frere x y)
+            (eqv? x y))
+
+   (define/public (soeur x y)
+            (eqv? x y))
+
+  (define/public (fils x y)
+            (eqv? x y))
+
+
+  (define/public (fille x y)
+            (eqv? x y))
+
+  (define/public (cousin x y)
+            (eqv? x y))
+
+
+  (define/public (neuveu x y)
+            (eqv? x y))
+            
+  )
 )
+
+
+;;construction de l'Arbre
+
+(define ArbreGenealogique%
+(class object%
+(super-new)
+
+  (define/public (pere x )
+    (cond 
+    [(eq? x "toto")(write "mankou")]
+      [(eq? x "ali")(write "toto")]
+      [(eq? x "yao")(write "ali")]
+      [(eq? x "manu")(write "ali")]
+      [(eq? x "fatou")(write "ali")]
+      [(eq? x "froto")(write "mankou")]
+      [(eq? x "pépé")(write "froto")]
+      [else "pas de papa"])
+    
+  )
+  
+
+  (define/public (ancetre x )
+    (cond 
+    [(eq? x "adjoua")(write "fitini,titi,toto,mankou,mamie")];a continuer avec mami 
+    [(eq? x "yao")(write "ali,toto,tata,mankou,mamie")]
+    [(eq? x "manu")(write "ali,toto,tata,mankou,mamie")]
+    [(eq? x "fatou")(write "ali,toto,tata,mankou,mamie")]
+    [(eq? x "ali")(write "toto,tata,mankou,mamie")]
+    [(eq? x "toto")(write "mankou,mamie")]
+    [(eq? x "mankou")(write "mamie")]
+    [else "pas d'ancetre"])
+    
+  )
+
+))
+  
+      
+
+  
+
 
 
 (write "**************************************")(newline)
 ;(send  (new Femme%) tata)
 
-(send (new Famille%) parent (send (new Homme%) toto "?")(send (new Homme%) toto "?"))
+;(send (new Famille%) neuveu(send (new Homme%) toto "?")(send (new Homme%) toto "?"))
 ;(send (new Homme%) toto "?")
 ;(send (new Femme%) tata "?")
+
+
+(send (new ArbreGenealogique%) ancetre "ali")
+
 
 
 
