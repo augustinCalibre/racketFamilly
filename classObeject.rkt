cad87e355cfd1a28021440466c166e9366fcf7c7
augustin <grahaugustin@gmail.com>
2021-05-06 22:59:48 +0000

    class Parents ok 22h59

--- classObeject.rkt
+++ classObeject.rkt
@@ -8,7 +8,6 @@
                   monocotyledone (phanérogame 1-cotyledone)
                   ))
 
-
 (class object%
   (super-new)
   (field [minestrone 'ready]
@@ -17,75 +16,46 @@
 
 
 
-;;(define famille%
-; (class object%
-;  (super-new)
-; (define (homme) 't)
-; (define (femme) 'femme)
-;  ))
-
-;(send (new famille%) homme)
 
 (define Femme%
   (class object%
      (super-new)
       ;(define tru #t)(define false #f)
     
-      (define/public (tata)(define tata #t)(write tata))
-  ))
+      (define/public (tata x)(eqv? x "?" )))
+      
+      ;(define tata #t)(write tata))
+  )
 
 (define Homme%
-  (class Femme%
+  (class object%
      (super-new)
-    (inherit  tata)
+    
       ;(define tru #t)(define false #f)
     
-      (define/public (toto)(define toto #t)(write toto))
-   ))
-
-
-
+      (define/public (toto x)(eqv? x "?" )))
+   )
 
 
 (define Famille%
-  (class Homme%
+  (class object%
     (super-new)
-    (inherit  toto)
-    (define (parent tata toto)
-            (if (eq? tata toto )
-            (write #t)
-            (write #f)
-                )
-            )))
-
-    
-    ;;Methode femme
-    ;;(define (femme x)
-    ;;  (if (eq? x #f )(write #t)(write #f)))
-        
   
-    ;;(public  parent )
-    ;;))
+    (define/public (parent x y)
+            (eqv? x y))))
 
 
-
-;;(send (new famille%)femme tata)
-;;(define x (new famille%))
-;;(send x femme tata)
-
-
-;;initialisation des hommes
-;(new  toto (new Homme%))
+  (define/public (parent x y)
+            (eqv? x y)
+)
 
 
+(write "**************************************")(newline)
+;(send  (new Femme%) tata)
 
-;;initialisation des hommes
-;(send (new Femme%) tata)
-
-(write "**************************************")
-;(send  (new Homme%) toto)
-
-; (send  (new Famille%) parent (send(new Homme%) toto)(send (new Femme%) tata))
+(send (new Famille%) parent (send (new Homme%) toto "?")(send (new Homme%) toto "?"))
+;(send (new Homme%) toto "?")
+;(send (new Femme%) tata "?")
 
 
 
